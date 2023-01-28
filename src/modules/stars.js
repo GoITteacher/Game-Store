94bed74deef10e09b18eaac3b202c732f16cc19d
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-01-28 16:37:09 +0200

    update

---
+++ src/modules/stars.js
@@ -0,0 +1,8 @@
+export function setRating(rating) {
+  const starList = document.querySelector('.star-rating__live');
+  for (let i = 0; i < Math.round(rating); i++) {
+    starList.children[i].classList.add('star-rating__item_active');
+  }
+
+  document.querySelector('.star-rating__avg').textContent = rating;
+}
--- src/scripts/game-info.js
+++ src/scripts/game-info.js
@@ -1,12 +1,30 @@
+import 'lazysizes';
+import 'lazysizes/plugins/parent-fit/ls.parent-fit';
+
 import { loadFromLS } from './helpers';
+import { setRating } from '../modules/stars';
 
+// ============================================
+
+const refs = {
+  loadMoreInfo: document.querySelector('.js-btn-more-info'),
+};
+
+const CONSTANTS = {
+  isFullInfo: false,
+};
+
+// ============================================
+
+refs.loadMoreInfo.addEventListener('click', e => {});
+
+// ============================================
 let currentGame;
-
 function onLoadPage() {
   currentGame = loadFromLS('currentGame');
   if (!currentGame) {
     window.location.pathname = '/';
   }
+  setRating(4.7);
 }
-
 onLoadPage();

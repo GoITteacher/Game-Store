d5d1a16d2da15f91d7d6e36ed5f8d07e755110c6
GoITteacher <50087029+GoITteacher@users.noreply.github.com>
2023-01-28 12:31:05 +0000

    Deploying to gh-pages from @ GoITteacher/Game-Store@31feb40aee98541ed1a56f29895ed72cb81121a1 🚀

---
+++ game-info.9b87e99f.js
@@ -0,0 +1,2 @@
+let t;t=function(t){const e=localStorage.getItem(t);try{return JSON.parse(e)}catch{return e||[]}}("currentGame"),t||(window.location.pathname="/");
+//# sourceMappingURL=game-info.9b87e99f.js.map

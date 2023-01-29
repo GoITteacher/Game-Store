bb96189025ebb2ed24860e3be96fed3487344985
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2023-01-29 22:00:24 +0200

    update

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-// export const HOST = '/Game-Store/';
-export const HOST = '/';
+export const HOST = '/Game-Store/';
+// export const HOST = '/';
 export const ACCESS_KEY = process.env.ACCESS_KEY;
 export const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY;
